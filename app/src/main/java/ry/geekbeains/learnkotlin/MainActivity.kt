5431e26463465b4ab4baa5549b4ba0bd17d95376
Left-slipper <62345840+Left-slipper@users.noreply.github.com>
2021-02-04 22:39:04 +0700

    created demo file for 5 lessons homework

--- app/src/main/java/ry/geekbeains/learnkotlin/MainActivity.kt
+++
@@ -1,9 +0,0 @@
-package ry.geekbeains.learnkotlin
-
-// дабвлю скоро задание, пока присылаю заглушку
-
-class MainActivity {
-
-    // КОТЛИН СЮДА, КОМЕНТЫ ДЛЯ ПУША
-
-}
---
+++ lessons5/app/src/main/java/ry/geekbeains/lessons5/MainActivity.kt
@@ -0,0 +1,5 @@
+package ry.geekbeains.lessons5
+
+class MainActivity {
+    // задание 5, скоро также сдам.
+}
